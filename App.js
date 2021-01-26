be683fbbb17d0990faaf3f503c5db1de32fcd355
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-01-26 12:19:29 +0800

    refactor: first commit

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import colors from "./config/colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.status__title}>Today</Text>
+        <Text style={styles.status__tasks}>0 tasks remaining</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrapper: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  status__title: {
+    color: colors.monoPrimary,
+    fontWeight: "700",
+    fontSize: 42,
+  },
+  status__tasks: {
+    color: colors.monoTinted,
+    fontWeight: "600",
+    fontSize: 18,
   },
 });

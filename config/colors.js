e99ece071dd3352ec6e7d147935fec1184b423f4
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-01-26 12:19:40 +0800

    feat: colors config

---
+++ config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#a9bcee",
+  primaryShaded: "#414f75",
+  primaryMuted: "#b5c1de",
+  monoPrimary: "#3d3d3d",
+  monoSecnodary: "#707070",
+  monoTinted: "#b7b7b7",
+  background: "#fafafa",
+  white: "#ffffff",
+};
